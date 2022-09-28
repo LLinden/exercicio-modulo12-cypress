89779cbf0eebebdb646bee1fa9af04f99cb9606c
Lucas Linden <lucas.linden@cwi.com.br>
2022-09-28 09:14:07 -0300

    usando page objects

--- cypress.config.js
+++ cypress.config.js
@@ -3,6 +3,7 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "http://lojaebac.ebaconline.art.br/",
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {},
   },
 });
--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,14 +1,32 @@
 /// <reference types="cypress" />
 
-describe('Funcionalidade Endereços - Faturamento e Entrega', () => {
-    beforeEach(() => {
-        cy.visit("minha-conta")
-        cy.fixture('perfil').then(dados => {
-            cy.login(dados.usuario, dados.senha)
-        })
+import EnderecoPage from "../support/page-objects/endereco.page";
+
+describe("Funcionalidade Endereços - Faturamento e Entrega", () => {
+  beforeEach(() => {
+    cy.visit("minha-conta");
+    cy.fixture("perfil").then((dados) => {
+      cy.login(dados.usuario, dados.senha);
     });
-    
-    it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-    });
+  });
+
+  it.only("Deve fazer cadastro de faturamento com sucesso", () => {
+    EnderecoPage.editarEnderecoFaturamento(
+      "Teste",
+      "Sobrenome",
+      "Ebac",
+      "Brasil",
+      "Av. Brasil",
+      "123",
+      "Taquara",
+      "Rio Grande do Sul",
+      "95600-000",
+      "99999999",
+      "email@dominio.com"
+    );
+    cy.get(".woocommerce-message").should(
+      "contain",
+      "Endereço alterado com sucesso."
+    );
+  });
 });
