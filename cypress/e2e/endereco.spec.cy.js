a94952a6862831f35b1fa021563e333e9a69d304
Lucas Linden <lucas.linden@cwi.com.br>
2022-09-28 09:25:09 -0300

    usando listas

--- cypress/e2e/endereco.spec.cy.js
+++ cypress/e2e/endereco.spec.cy.js
@@ -1,6 +1,7 @@
 /// <reference types="cypress" />
 
 import EnderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require("../fixtures/endereco.json");
 
 describe("Funcionalidade Endereços - Faturamento e Entrega", () => {
   beforeEach(() => {
@@ -10,7 +11,7 @@ describe("Funcionalidade Endereços - Faturamento e Entrega", () => {
     });
   });
 
-  it.only("Deve fazer cadastro de faturamento com sucesso", () => {
+  it("Deve fazer cadastro de faturamento com sucesso", () => {
     EnderecoPage.editarEnderecoFaturamento(
       "Teste",
       "Sobrenome",
@@ -23,6 +24,26 @@ describe("Funcionalidade Endereços - Faturamento e Entrega", () => {
       "95600-000",
       "99999999",
       "email@dominio.com"
+    );
+    cy.get(".woocommerce-message").should(
+      "contain",
+      "Endereço alterado com sucesso."
+    );
+  });
+
+  it.only("Deve fazer cadastro de faturamento com sucesso usando arquivo de dados", () => {
+    EnderecoPage.editarEnderecoFaturamento(
+      dadosEndereco[1].nome,
+      dadosEndereco[1].sobrenome,
+      dadosEndereco[1].empresa,
+      dadosEndereco[1].pais,
+      dadosEndereco[1].endereco,
+      dadosEndereco[1].numero,
+      dadosEndereco[1].cidade,
+      dadosEndereco[1].estado,
+      dadosEndereco[1].cep,
+      dadosEndereco[1].telefone,
+      dadosEndereco[1].email
     );
     cy.get(".woocommerce-message").should(
       "contain",
